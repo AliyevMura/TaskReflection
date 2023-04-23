bb579c9665cb0dc21909c970c366ca1c2bbe551f
AliyevMura <7lvghl9@code.edu.az>
2023-04-23 22:25:40 +0400

    Initial commit

---
+++ TaskReflection/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskReflection.Models
+{
+    public class User
+    {
+
+
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private static int Age { get; set; }
+
+
+
+        public void GetFullName()
+        {
+            Console.WriteLine(Name + " " + Surname);
+        }
+
+
+        public static void ChangeAge(int age)
+        {
+            Console.WriteLine(Age = age);
+
+        }
+    }
+}
---
+++ TaskReflection/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using TaskReflection.Models;
+
+namespace TaskReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(User);
+
+
+            User person = new User();
+            PropertyInfo propId = type.GetProperty("Id", BindingFlags.Instance | BindingFlags.NonPublic);
+            propId.SetValue(person, 4);
+            Console.WriteLine(propId.GetValue(person));
+
+
+            PropertyInfo propName = type.GetProperty("Name", BindingFlags.Instance | BindingFlags.NonPublic);
+            propName.SetValue(person, "Murad");
+            Console.WriteLine(propName.GetValue(person));
+
+            PropertyInfo propSurname = type.GetProperty("Surname", BindingFlags.Instance | BindingFlags.NonPublic);
+            propSurname.SetValue(person, "Eliyev");
+            Console.WriteLine(propSurname.GetValue(person));
+
+
+            PropertyInfo propAge = type.GetProperty("Age", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+            propAge.SetValue(person, 21);
+            Console.WriteLine(propAge.GetValue(person));
+
+            Console.WriteLine("=====================================");
+
+
+
+
+            MethodInfo methodGetFullName = type.GetMethod("GetFullName", BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            methodGetFullName.Invoke(person, null);
+
+
+
+
+            MethodInfo methodChangeAge = type.GetMethod("ChangeAge", BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            methodChangeAge.Invoke(person, new object[] { 30 });
+        }
+    }
+}
